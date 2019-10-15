5c7409e315212f8b3ec0ba2434230d165ffd6135
Kai Page <kaibug@gmail.com>
2019-10-15 18:34:25 -0500

    Starting on webhook handler

--- bin/webhook.dart
+++ bin/webhook.dart
@@ -1,5 +1,51 @@
+import 'dart:collection';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math' as math;
+
+import 'package:async/async.dart';
+import 'package:collection/collection.dart';
+import 'package:convert/convert.dart';
+import 'package:crypto/crypto.dart';
 import 'package:shelf/shelf.dart';
 
+final _secureRand = math.Random.secure();
+
 Future<Response> handleWebhook(Request request) async {
-  return Response(501); // not implemented
+  var body = await collectBytes(request.read());
+
+  // verify X-Hub-Signature
+  try {
+    verifyKey(request.headers["x-hub-signature"], body);
+  } on Response catch (r) {
+    // don't leak timing information
+    // this is apparently v important
+    await Future.delayed(
+        Duration(microseconds: _secureRand.nextInt(100 * 1000)));
+    return r;
+  }
+
+  print("got an event: ${request.headers["x-github-event"]}");
+  print(utf8.decode(body));
+
+  return Response(204);
+}
+
+final _sig = RegExp("sha1=([0-9A-Fa-f]{40})");
+final _key = UnmodifiableListView(
+    utf8.encode(Platform.environment["WEBHOOK_SECRET"]));
+final _hmac = Hmac(sha1, _key);
+const _lequal = ListEquality();
+
+void verifyKey(String xHubSignature, List<int> body) {
+  var match = _sig.firstMatch(xHubSignature);
+  if (match == null) {
+    throw Response(400, body: "bad X-Hub-Signature header");
+  }
+
+  var sigBytes = hex.decode(match.group(1));
+  var digest = _hmac.convert(body);
+  if (!_lequal.equals(sigBytes, digest.bytes)) {
+    throw Response.forbidden("incorrect signature, nice try");
+  }
 }
